2f6cc826abe5633b6620fbc963492d223e542c9d
Helique <davidbernadett@sbcglobal.net>
2016-03-29 20:21:18 -0700

    Added some front page text.

--- cluster/app.js
+++ cluster/app.js
@@ -2,15 +2,21 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var routes = require('./routes/index');
+var nunjucks = require('nunjucks');
 
 //set routes
-app.get('/', function(req, res){
-	res.json({result: "result"});
-});
 
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app,
+	watch: true
+});
+app.use("/", routes);
+//app.use("/static", express.static("public"));
 //views as directory for all template files
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'nunjucks');
 // instruct express to serve up static assets
 app.use(express.static('public'));
 module.exports = app;
