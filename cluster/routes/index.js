0b508796b9e9b4bfe43f178516e0d8536cc59cf1
= <=>
2016-06-26 00:41:11 -0400

    added a simple file uploader

--- cluster/routes/index.js
+++ cluster/routes/index.js
@@ -1,11 +1,30 @@
 var express = require('express');
+var multer  =   require('multer');
 var router = express.Router();
-
+var storage =   multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './uploads');
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now());
+    }
+});
+var upload = multer({ storage : storage}).single('userPhoto');
 /* GET home page */
 
 router.get('/', function(req, res, next){
 	res.render('index.nunjucks');
 	//res.json({result: "result"});
+});
+
+router.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            console.log(err);
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
 });
 
 module.exports = router;
