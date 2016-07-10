d5873efb5876a3aa8a3540376fa7c6be74cc37bb
= <=>
2016-07-10 02:23:30 -0400

    The transaction history files are parsed and the database is updated

--- cluster/scripts/create_database.js
+++ cluster/scripts/create_database.js
@@ -31,14 +31,18 @@ CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.regex_table + '` ( \
 connection.query('\
 CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.charges_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
-    `charge` INT NOT NULL, \
     `description` CHAR(60) NOT NULL, \
+    `charge` DOUBLE NOT NULL, \
+    `memo` CHAR(60) NOT NULL, \
+    `fitid` INT NOT NULL, \
     `category_id` INT UNSIGNED NOT NULL, \
     `date` DATE NOT NULL, \
+    `acc_balance` DOUBLE NOT NULL, \
         PRIMARY KEY (`id`), \
     UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
     INDEX `category_id_INDEX` (`category_id` ASC), \
-    INDEX `date_INDEX` (`date` ASC) \
+    INDEX `date_INDEX` (`date` ASC), \
+    UNIQUE INDEX `fitid_UNIQUE` (`fitid` ASC) \
 )');
 
 console.log('Success: Database Created!')
