66a5d08ca9fc21b2fd6198df11d5684a8bb665e0
helique <davidbernadett@sbcglobal.net>
2016-08-21 19:43:50 -0400

    closes #4 by adding a filter for dates and inputs for the filters. Also increments the month by one to fix bug

--- cluster/public/charge-list/charge-list.component.js
+++ cluster/public/charge-list/charge-list.component.js
@@ -21,4 +21,36 @@ angular.
             self.accountService = accountService;
             self.ordering = '-date';
         }
+    })
+    .filter('dateBetween', function() {
+        // In the return function, we must pass in a single parameter which will be the data we will work on.
+        // We have the ability to support multiple other parameters that can be passed into the filter optionally
+        return function(input, dateStart, dateEnd) {
+            var output = [];
+            if (dateEnd != undefined && dateStart != undefined){
+                angular.forEach(input, function(value, key) {
+                    if(dateStart <= value.date && dateEnd >= value.date) {
+                        output.push(value);
+                    }
+                });
+            }else if(dateEnd != undefined){
+                angular.forEach(input, function(value, key) {
+                    if(dateEnd >= value.date) {
+                        output.push(value);
+                    }
+                });
+            }else if(dateStart != undefined){
+                angular.forEach(input, function(value, key) {
+                    if(dateStart <= value.date) {
+                        output.push(value);
+                    }
+                });
+            } else {
+                output = input;
+            }
+            console.log(dateStart);
+            console.log(dateEnd);
+            return output;
+
+        }
     });
