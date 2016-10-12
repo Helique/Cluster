d2e8eecd397c182866dde652efb7786301966948
Porter <pmh192@gmail.com>
2016-10-12 03:25:05 +0000

    In generate_credentials.sh make dir before copying into it

--- cluster/scripts/generate_credentials.sh
+++ cluster/scripts/generate_credentials.sh
@@ -1,1 +1,5 @@
-cp -r ../config/credential_templates/* ../config/credentials/.
+target="../config/credentials"
+if [ ! -d "$target" ]; then
+   mkdir "$target"
+fi
+cp -r ../config/credential_templates/* "$target"
